2ec23b777e436b636a2efd92a25c01a525d4e312
arabog <arababs2015@gmail.com>
2023-02-09 19:31:50 +0100

    modify: learning about test

--- test.py
+++ test.py
@@ -1,31 +1,24 @@
+'''
+def test_sum():
+          assert sum([1, 2, 3]) == 6, "Should be 6"
+
+def test_sum_tuple():
+          assert sum((1, 2, 2)) == 6, "Should be 6"
+
+if __name__ == "__main__":
+          test_sum()
+          test_sum_tuple()
+          print("Everything passed")
+'''
 import unittest
 
-from project.my_sum import sum
-from fractions import Fraction
-
 class TestSum(unittest.TestCase):
-          def test_list_int(self):
-                    """
-                    Test that it can sum a list of integers
-                    """
-                    data = [1, 2, 3]
-                    result = sum(data)
-                    self.assertEqual(result, 6)
 
-          def test_list_fractions(self):
-                    """
-                    Test that it can sum a list of fractions
-                    """
-                    data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
-                    result = sum(data)
-                    self.assertEqual(result, 1)
+          def test_sum(self):
+                    self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
 
-
-          def test_bad_type(self):
-                    data = "banana"
-                    with self.assertRaises(TypeError):
-                              result = sum(data)
-
+          def test_sum_tuple(self):
+                    self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
 
 if __name__ == "__main__":
           unittest.main()
